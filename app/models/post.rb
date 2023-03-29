52d1f40280a5cc235fec0dd92bd8b5bf76b6f82f
gauravgangwarweb <gauravgangwar501@gmail.com>
2023-03-29 20:52:47 +0530

    Fix linter errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,13 @@
 class Post < ApplicationRecord
-    belongs_to :user
-    has_many :likes
-    has_many :comments
+  belongs_to :user
+  has_many :likes
+  has_many :comments
 
-    def update_users_posts_counter
-        user.increment(:posts_counter)
-    end
+  def update_users_posts_counter
+    user.increment(:posts_counter)
+  end
 
-    def most_recent_comments
-        comments.order(created_at: :desc).limit(5)
-    end
+  def most_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
