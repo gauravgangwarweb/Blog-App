9a0a04b3524129878d4748779212802f710ebb12
gauravgangwarweb <gauravgangwar501@gmail.com>
2023-03-29 20:51:31 +0530

    process data in models done

---
+++ app/models/post.rb
@@ -0,0 +1,13 @@
+class Post < ApplicationRecord
+    belongs_to :user
+    has_many :likes
+    has_many :comments
+
+    def update_users_posts_counter
+        user.increment(:posts_counter)
+    end
+
+    def most_recent_comments
+        comments.order(created_at: :desc).limit(5)
+    end
+end
