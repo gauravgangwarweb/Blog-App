0c768ed5eced836ea29df845832f2bc606180f89
gauravgangwarweb <gauravgangwar501@gmail.com>
2023-03-30 20:23:33 +0530

    Add spec files

--- db/schema.rb
+++ db/schema.rb
@@ -40,8 +40,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_28_131803) do
     t.integer "likes_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "users_id", null: false
-    t.index ["users_id"], name: "index_posts_on_users_id"
+    t.bigint "author_id"
+    t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
