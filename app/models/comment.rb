418cd793d449484d89d3c957d4b4c3e9a668c5f1
gauravgangwarweb <gauravgangwar501@gmail.com>
2023-03-30 12:54:25 +0530

    Add required optional chnages

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
+  after_save :update_comments_posts_counter
+
+  private
 
   def update_comments_posts_counter
     post.increment!(:comments_counter)
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,10 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
+  
+  after_save :update_users_posts_counter
+
+  private
 
   def update_users_posts_counter
     user.increment(:posts_counter)
