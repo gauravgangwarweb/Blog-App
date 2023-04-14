5cfe5bf0d63932a8b259834ecac0ceecd2fa337f
gauravgangwarweb <gauravgangwar501@gmail.com>
2023-04-14 13:39:22 +0530

    Add role based functanality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,23 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  load_and_authorize_resource
   def index
     @users = User.all
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.posts.destroy_all
+    if user.destroy
+      flash[:success] = 'User deleted successfully'
+      redirect_to users_path()
+    else
+      flash.now[:error] = 'Error: User could not be deleted'
+      redirect_to user_path(user)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'users#index'
-  resources :users, only: [:index, :show] do 
-    resources :posts, only: [:index, :show, :new, :create] do 
-      resources :comments, only: [ :new, :create]
+  resources :users, only: [:index, :show, :destroy] do 
+    resources :posts, only: [:index, :show, :new, :create, :destroy] do 
+      resources :comments, only: [ :new, :create, :destroy]
       resources :likes, only: [:create]
     end
   end
